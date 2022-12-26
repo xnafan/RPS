983b2abecae7019b82428708f6c976c255cdfda9
Jakob Farian Krarup <kr@rup.dk>
2022-12-26 01:11:37 +0100

    Simplified and cleaned code

--- RPS/RpsGame.cs
+++ RPS/RpsGame.cs
@@ -7,25 +7,25 @@ using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-
 namespace RPS;
 public class RpsGame : Game
 {
+    #region Properties
     private const double _speed = .1;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private List<GameObject> _playerObjects = new();
     private readonly Random rnd = new();
     public static Texture2D Paper, Rock, Scissors;
-    public static Rectangle GameBounds { get; set; } = new Rectangle(0,0,1920, 1080);
-    public static Dictionary<RpsType, Texture2D> TypeTextures { get; set; } = new ();
+    public static Rectangle GameBounds { get; set; } = new Rectangle(0, 0, 1920, 1080);
+    public static Dictionary<RpsType, Texture2D> TypeTextures { get; set; } = new();
     private SpriteFont _font;
-    private string _status = "";
-    private static StringBuilder _builder = new StringBuilder ();
-    private GamePartitioningHelper<GameObject> _partitioningHelper = new GamePartitioningHelper<GameObject>(GameBounds,6);
-    private RpsType[] _typeToConvertTo;
-    private int _numberOfGameObjects = 1000;
-    Stopwatch _stopWatch;
+    private static StringBuilder _builder = new StringBuilder();
+    private GamePartitioningHelper<GameObject> _partitioningHelper = new GamePartitioningHelper<GameObject>(GameBounds, 5);
+    private int _numberOfGameObjects = 400;
+    #endregion
+
+    #region Constructor and initialization
     public RpsGame()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -34,24 +34,21 @@ public class RpsGame : Game
         _graphics.IsFullScreen = true;
         _graphics.ApplyChanges();
         Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-
+        IsMouseVisible = false;
     }
-
     public void NewGame()
     {
         _playerObjects.Clear();
-        _typeToConvertTo = new RpsType[_numberOfGameObjects];
         for (int i = 0; i < _numberOfGameObjects; i++)
         {
-            AddRandomPlayerObject();
+            var location = new Vector2(rnd.Next(_graphics.PreferredBackBufferWidth), rnd.Next(_graphics.PreferredBackBufferHeight));
+
+            _playerObjects.Add(new GameObject(TypeTextures[RpsType.Rock].Width) { Location = location, Speed = _speed });
         }
     }
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-        //_texture = CreateTexture(GraphicsDevice, 32, 32, pixel => Color.White);
-        
         Rock = Content.Load<Texture2D>("gfx/Rock_32px");
         Paper = Content.Load<Texture2D>("gfx/Paper_32px");
         Scissors = Content.Load<Texture2D>("gfx/Scissors_32px");
@@ -60,107 +57,61 @@ public class RpsGame : Game
         TypeTextures.Add(RpsType.Scissors, Scissors);
         _font = Content.Load<SpriteFont>("Font");
         NewGame();
-    } 
-
-    private void AddRandomPlayerObject()
-    {
-        var location = new Vector2(rnd.Next(_graphics.PreferredBackBufferWidth), rnd.Next(_graphics.PreferredBackBufferHeight));
-
-        _playerObjects.Add(new GameObject(TypeTextures[RpsType.Rock].Width) { Location = location, Speed = _speed });
     }
+
+    #endregion
 
     protected override void Update(GameTime gameTime)
     {
-        _stopWatch = Stopwatch.StartNew();
-        if (Keyboard.GetState().IsKeyDown(Keys.Escape))Exit();
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
         if (Keyboard.GetState().IsKeyDown(Keys.F5)) NewGame();
-        if (Keyboard.GetState().IsKeyDown(Keys.Add)) { _numberOfGameObjects += 100; NewGame(); };
-        if (Keyboard.GetState().IsKeyDown(Keys.Subtract)) { _numberOfGameObjects -= 100; NewGame(); };
 
         _partitioningHelper.Update(_playerObjects);
-
-        int index = 0;
         foreach (var obj in _playerObjects)
         {
             obj.Update(gameTime);
             var collisions = _partitioningHelper.GetCollisions(obj);
-            //Debug.WriteLine("collisions:" + collisions.Count());
-            foreach (var collisionItem in collisions)
-            //var collisionItem = collisions.FirstOrDefault();
-            //if(collisionItem!= null)
+            var collisionItem = collisions.FirstOrDefault(colObj => colObj.RpsType != obj.RpsType);
+            if (collisionItem != null)
             {
-                if(obj.RpsType == RpsType.Rock && collisionItem.RpsType == RpsType.Paper) { _typeToConvertTo[index] = RpsType.Paper;}
-                else if (obj.RpsType == RpsType.Paper && collisionItem.RpsType == RpsType.Scissors) { _typeToConvertTo[index] = RpsType.Scissors;  }
-                else if (obj.RpsType == RpsType.Scissors&& collisionItem.RpsType == RpsType.Rock) { _typeToConvertTo[index] = RpsType.Rock;}
+                if (obj.RpsType == RpsType.Rock && collisionItem.RpsType == RpsType.Paper) { obj.RpsType = RpsType.Paper; _partitioningHelper.Remove(obj); }
+                else if (obj.RpsType == RpsType.Paper && collisionItem.RpsType == RpsType.Scissors) { obj.RpsType = RpsType.Scissors; _partitioningHelper.Remove(obj); }
+                else if (obj.RpsType == RpsType.Scissors && collisionItem.RpsType == RpsType.Rock) { obj.RpsType = RpsType.Rock; _partitioningHelper.Remove(obj); }
             }
-            index++;
-
-              //  _partitioningHelper.Remove(obj); 
-
         }
-        for (int i = 0; i < _numberOfGameObjects; i++)
+    }
+
+    #region Draw and related
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+        _spriteBatch.Begin();
+        _playerObjects.ForEach(obj => obj.Draw(_spriteBatch, gameTime));
+        WriteStatus();
+        _spriteBatch.End();
+    }
+    private void WriteStatus()
+    {
+        var count = _playerObjects.GroupBy(x => x.RpsType).Select(group => new
         {
-            _playerObjects[i].RpsType = _typeToConvertTo[i];
-        }
-        var count = _playerObjects.GroupBy(x => x.RpsType).Select(group => new {
             Metric = group.Key,
             Count = group.Count()
         }).ToList();
 
         _builder.Clear();
         count.ForEach(c => _builder.Append(c.Metric + ":" + c.Count + Environment.NewLine));
-        _status = _builder.ToString();
-        _stopWatch.Stop();
-        Debug.WriteLine("update: " + _stopWatch.ElapsedMilliseconds);
+        DrawWithOutline(_builder.ToString());
     }
-
-    
-    protected override void Draw(GameTime gameTime)
+    private void DrawWithOutline(string text)
     {
-        _stopWatch = new Stopwatch();
-        GraphicsDevice.Clear(Color.Black);
-        _spriteBatch.Begin();
-        _playerObjects.ForEach(obj => obj.Draw(_spriteBatch, gameTime));
-        DrawWithOutline();
-        _spriteBatch.End();
-        _stopWatch.Stop();
-        Debug.WriteLine("draw: " + _stopWatch.ElapsedMilliseconds);
-    }
-
-    private void DrawWithOutline()
-    {
-
         for (int x = -1; x <= 1; x++)
         {
             for (int y = -1; y <= 1; y++)
             {
-        _spriteBatch.DrawString(_font, _status, Vector2.One * 10 + new Vector2(x,y), Color.Black);
+                _spriteBatch.DrawString(_font, text, Vector2.One * 10 + new Vector2(x, y), Color.Black);
             }
         }
-        _spriteBatch.DrawString(_font, _status, Vector2.One * 10, Color.White);
-    }
-
-    public static Texture2D CreateTexture(GraphicsDevice device, int width, int height, Func<int, Color> paint)
-    {
-        //initialize a texture
-        Texture2D texture = new Texture2D(device, width, height);
-
-        //the array holds the color for each pixel in the texture
-        Color[] data = new Color[width * height];
-        for (int pixel = 0; pixel < data.Count(); pixel++)
-        {
-            //the function applies the color according to the specified pixel
-            data[pixel] = paint(pixel);
-        }
-
-        //set the color
-        texture.SetData(data);
-
-        return texture;
-    }
-}
-
-public static class RectangleExtensions
-{
-    
+        _spriteBatch.DrawString(_font, text, Vector2.One * 10, Color.White);
+    } 
+    #endregion
 }
